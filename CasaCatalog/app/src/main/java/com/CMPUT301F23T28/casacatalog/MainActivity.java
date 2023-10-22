f74751a0fb2107b5c06f59e03fd867bb60d4f762
Ryan Bott <bott.ryan@gmail.com>
2023-10-22 16:47:27 -0600

    Project initial repo

---
+++ CasaCatalog/app/src/main/java/com/CMPUT301F23T28/casacatalog/MainActivity.java
@@ -0,0 +1,14 @@
+package com.CMPUT301F23T28.casacatalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
