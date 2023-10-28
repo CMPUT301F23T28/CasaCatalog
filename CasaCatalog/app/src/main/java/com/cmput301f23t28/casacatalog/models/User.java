4f6ca1e7a058f0951adccd099613971b9c97e7cc
Ervin Lanada <ervinjon_lanada@outlook.com>
2023-10-28 13:04:34 -0600

    Add User class

---
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/models/User.java
@@ -0,0 +1,17 @@
+package com.cmput301f23t28.casacatalog.models;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
