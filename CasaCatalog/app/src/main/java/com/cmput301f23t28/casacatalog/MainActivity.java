58868f04d7e8648e97804d17ac31280ff97d3170
Jordzira <69765875+Jordzira@users.noreply.github.com>
2023-10-28 12:31:28 -0600

    Update MainActivity.java

--- CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/MainActivity.java
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/MainActivity.java
@@ -1,15 +1,41 @@
 package com.cmput301f23t28.casacatalog;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private FirebaseFirestore db;
+    private CollectionReference itemsRef;
+    private ArrayList<String> dataList;
+    private ArrayAdapter<String> itemAdapter;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+
+        itemsRef = db.collection("items");
+
+        dataList = new ArrayList<>();  // Other code omitted }
+
+        dataList.add("Test");
+        itemAdapter.notifyDataSetChanged();
+
+
     }
 
 }
