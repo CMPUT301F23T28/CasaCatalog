b892715631a3aec79737d8f5895260af26969691
Ryan Bott <bott.ryan@gmail.com>
2023-11-05 12:05:56 -0700

    MainActivity draft

--- CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/views/MainActivity.java
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/views/MainActivity.java
@@ -1,8 +1,10 @@
-package com.cmput301f23t28.casacatalog;
+package com.cmput301f23t28.casacatalog.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.cmput301f23t28.casacatalog.R;
 
 public class MainActivity extends AppCompatActivity {
 
