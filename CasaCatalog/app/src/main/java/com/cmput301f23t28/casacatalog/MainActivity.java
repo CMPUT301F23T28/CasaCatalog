1fad21fd19d7110ffea64972352a56f80cbac417
Jordzira <69765875+Jordzira@users.noreply.github.com>
2023-11-05 13:44:05 -0700

    Updated add method

--- CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/MainActivity.java
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/MainActivity.java
@@ -28,7 +28,6 @@ import com.cmput301f23t28.casacatalog.models.Item;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<Item> itemList;
     private ListView itemListView;
 
     private ItemHandler itemHandler;
@@ -41,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
 
         itemHandler = new ItemHandler();
 
-//        itemList = new ArrayList<Item>();
         String[] items = {
                 "Chair", "Table"
         };
@@ -56,13 +54,16 @@ public class MainActivity extends AppCompatActivity {
         itemTest.setName(itemS);
         itemTest.setPrice(amount);
         itemHandler.addItem(itemTest);
-        itemHandler.deleteItem(itemTest);
+//        Log.e("TEst id", itemTest.getId());
+//        itemHandler.deleteItem(itemTest);
 
 
 
         itemAdapter = new ItemListAdapter(this, itemHandler.getItemList());
         itemListView = findViewById(R.id.items_list);
         itemListView.setAdapter(itemAdapter);
+
+
 
     }
 }
