17beaf225abbc5b4cfd4ebb58fd89ef2fe7395f1
Jordzira <69765875+Jordzira@users.noreply.github.com>
2023-11-08 17:13:18 -0700

    Update DatabaseHandler.java

--- CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/database/DatabaseHandler.java
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/database/DatabaseHandler.java
@@ -13,11 +13,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-
 import java.util.HashMap;
-import java.util.UUID;
-
-
 
 public class DatabaseHandler {
     private FirebaseFirestore db;
