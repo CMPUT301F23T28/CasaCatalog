e5711f0b11a956faae6259bc8498f836a7324263
jadc <chehimj@gmail.com>
2023-11-09 16:08:16 -0700

    accidentally overwrote too much

--- CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/views/AddItemActivity.java
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/views/AddItemActivity.java
@@ -3,6 +3,8 @@ package com.cmput301f23t28.casacatalog.views;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,13 +14,23 @@ import com.cmput301f23t28.casacatalog.R;
 import com.cmput301f23t28.casacatalog.models.Item;
 import com.cmput301f23t28.casacatalog.models.ItemHandler;
 import com.cmput301f23t28.casacatalog.models.Tag;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textview.MaterialTextView;
 
+import java.text.ParseException;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.text.SimpleDateFormat;
+
 
 public class AddItemActivity  extends AppCompatActivity {
 
     private ItemHandler itemHandler;
+    private Item newItem;
 
     /*
     //Add when it's also in the database (not sure if it is)
@@ -32,49 +44,97 @@ public class AddItemActivity  extends AppCompatActivity {
     private TextInputEditText itemComments = findViewById(R.id.itemComments);
     private TextInputEditText itemTags = findViewById(R.id.itemTags);
     */
-    // Needs photos too (not on UI yet)
-    Item newItem = new Item();
+        // Needs photos too (not on UI yet)
 
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_item);
-        // Need to reference the database from a different activity.
-        // This is kind of a bad way of doing this but if itemhandler always references the same
-        // database I don't think it should matter if I create a new one. Ideally this would
-        // probably reference the same itemhandler as mainactivity but this should work
-        itemHandler = new ItemHandler();
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_add_item);
+            // Need to reference the database from a different activity.
+            // This is kind of a bad way of doing this but if itemhandler always references the same
+            // database I don't think it should matter if I create a new one. Ideally this would
+            // probably reference the same itemhandler as mainactivity but this should work
+            itemHandler = new ItemHandler();
+            newItem = new Item();
 
-        findViewById(R.id.addItemToListBtn).setOnClickListener(view -> {
-            // Feel free to get rid of below if it's not necessary (the javadoc)
-            /**
-             * Adds item to database, as well as the item list displayed in MainActivity.
-             */
-            TextInputLayout itemName = findViewById(R.id.itemName);
-            newItem.setName(itemName.getEditText().getText().toString());
-            // Should check if value is actually a double (probably possible in EditText somehow)
-            TextInputLayout itemValue = findViewById(R.id.itemEstimatedValue);
-            if (itemValue.getEditText().getText().toString() != null) {
-                double price = Double.parseDouble(itemValue.getEditText().getText().toString());
-                newItem.setPrice(price);
-            }
+            final Button addButton = findViewById(R.id.addItemToListBtn);
 
-            // Add rest of attributes as well
+            addButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Feel free to get rid of below if it's not necessary (the javadoc)
+                    /**
+                     * Adds item to database, as well as the item list displayed in MainActivity.
+                     */
+//                TextInputLayout itemName = findViewById(R.id.itemName);
+
+                    TextInputLayout nameValue = findViewById(R.id.itemName);
+                    if (nameValue.getEditText().getText().toString() != null) {
+                        newItem.setName(nameValue.getEditText().getText().toString());
+                    }
 
 
-            itemHandler.addItem(newItem);
+                    // adds the price
+                    TextInputLayout itemValue = findViewById(R.id.itemEstimatedValue);
+                    if (!itemValue.getEditText().getText().toString().isEmpty()) {
+                        double price = Double.parseDouble(itemValue.getEditText().getText().toString());
+                        newItem.setPrice(price);
+                    }
 
-            finish();
-        });
+                    // adds the date
+                    TextInputLayout dateValue = findViewById(R.id.itemPurchaseDate);
+                    if (!dateValue.getEditText().getText().toString().isEmpty()) {
+                        SimpleDateFormat formatter = new SimpleDateFormat("dd-mm-yyyy", Locale.ENGLISH);
+                        try{
+                            Date date = formatter.parse(dateValue.getEditText().getText().toString());
+                            newItem.setDate(date);
+                        } catch (ParseException e){
+                            Log.e("ParseException", "ParseException"+ e.toString());
+                        }
 
-        // Add tag button that launches TagsActivity
-        // TODO: reuse this for when EditItemActivity is working
-        findViewById(R.id.addTagButton).setOnClickListener(view -> {
-            Intent i = new Intent(this, EditTagsActivity.class);
-            i.putExtra("tags", newItem.getTags());
-            // TODO: dont use deprecated method
-            startActivityForResult(i, 200);
-        });
-    }
+                    }
+
+                    // Add rest of attributes as well
+                    TextInputLayout makeValue = findViewById(R.id.itemMake);
+                    if (makeValue.getEditText().getText().toString() != null) {
+                        newItem.setMake(makeValue.getEditText().getText().toString());
+                    }
+
+                    TextInputLayout modelValue = findViewById(R.id.itemModel);
+                    if (modelValue.getEditText().getText().toString() != null) {
+                        newItem.setModel(modelValue.getEditText().getText().toString());
+                    }
+
+                    TextInputLayout descriptionValue = findViewById(R.id.itemDescription);
+                    if (descriptionValue.getEditText().getText().toString() != null) {
+                        newItem.setDescription(descriptionValue.getEditText().getText().toString());
+                    }
+
+                    TextInputLayout commentValue = findViewById(R.id.itemComments);
+                    if (commentValue.getEditText().getText().toString() != null) {
+                        newItem.setComment(commentValue.getEditText().getText().toString());
+                    }
+
+                    TextInputLayout serialNumberValue = findViewById(R.id.itemSerialNumber);
+                    if (serialNumberValue.getEditText().getText().toString() != null) {
+                        newItem.setSerialNumber(serialNumberValue.getEditText().getText().toString());
+                    }
+
+
+                    itemHandler.addItem(newItem);
+
+                    finish();
+                }
+            });
+
+            // Add tag button that launches TagsActivity
+            // TODO: reuse this for when EditItemActivity is working
+            findViewById(R.id.addTagButton).setOnClickListener(view -> {
+                Intent i = new Intent(this, EditTagsActivity.class);
+                i.putExtra("tags", newItem.getTags());
+                // TODO: dont use deprecated method
+                startActivityForResult(i, 200);
+            });
+        }
 
     // Receive data from EditTagsActivity
     @Override
@@ -96,4 +156,5 @@ public class AddItemActivity  extends AppCompatActivity {
 
         }
     }
+
 }
