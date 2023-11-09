bb47d38f58a3362d87a5f85c9c0041397df29ca1
Jordzira <69765875+Jordzira@users.noreply.github.com>
2023-11-08 17:20:37 -0700

    Update DatabaseHandler.java

--- CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/database/DatabaseHandler.java
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/database/DatabaseHandler.java
@@ -30,15 +30,16 @@ public class DatabaseHandler {
         itemsRef = db.collection("itemList");
     }
 
+
+    public CollectionReference getItemsRef() {
+        return itemsRef;
+    }
+
     /**
      * Adds an item to the Firestore database.
      *
      * @param item The Item object to be added.
      */
-    public CollectionReference getItemsRef() {
-        return itemsRef;
-    }
-
     public void addItemDatabase(Item item) {
 
         HashMap<String, Object> data = new HashMap<>();
