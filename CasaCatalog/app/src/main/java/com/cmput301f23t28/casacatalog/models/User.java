0b2cb4710e3d2ec989ed4c90fa92fd0b62267e0b
Ervin Lanada <ervinjon_lanada@outlook.com>
2023-11-09 18:34:40 -0700

    Add Java Docs for User.java

--- CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/models/User.java
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/models/User.java
@@ -1,16 +1,31 @@
 package com.cmput301f23t28.casacatalog.models;
 
+/**
+ * Represents a user with a name property.
+ */
 public class User {
     private String name;
 
+    /**
+     * Constructor for creating a new User with a given name.
+     * @param name A string representing the user's name.
+     */
     public User(String name) {
         this.name = name;
     }
 
+    /**
+     * Retrieves the name of the user.
+     * @return A string representing the user's name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets or updates the name of the user.
+     * @param name A string containing the new name of the user.
+     */
     public void setName(String name) {
         this.name = name;
     }
