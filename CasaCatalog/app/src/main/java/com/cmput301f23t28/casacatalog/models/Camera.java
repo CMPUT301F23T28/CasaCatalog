e7481f05f155c29142bfed8334ef7e59c9b17c2e
Jordzira <69765875+Jordzira@users.noreply.github.com>
2023-11-26 11:36:38 -0700

    .

---
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/models/Camera.java
@@ -0,0 +1,10 @@
+package com.cmput301f23t28.casacatalog.models;
+
+//Recognize text in images with ML Kit on Android
+//https://developers.google.com/ml-kit/vision/text-recognition/v2/android#java
+
+//Scan barcodes with ML Kit on Android
+//https://developers.google.com/ml-kit/vision/barcode-scanning/android
+
+public class Camera {
+}
