99da3a92ef278bb16db0635964504b437bdba891
Ervin Lanada <ervinjon_lanada@outlook.com>
2023-11-26 12:16:31 -0700

    Add Text Recognizer dependency and create class

---
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/helpers/TextRecognitionHelper.java
@@ -0,0 +1,9 @@
+package com.cmput301f23t28.casacatalog.helpers;
+
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+public class TextRecognitionHelper {
+    TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+}
