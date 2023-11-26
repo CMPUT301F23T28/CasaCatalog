84a92f0d4a99f17fea6d30c09ab2979ab6eb5e68
Ervin Lanada <ervinjon_lanada@outlook.com>
2023-11-26 14:32:43 -0700

    Comment out some code for now for PR

--- CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/Camera/BarcodeScanner.java
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/Camera/BarcodeScanner.java
@@ -1,10 +1,10 @@
-package com.cmput301f23t28.casacatalog.Camera;
-import com.google.mlkit.vision.barcode.Barcode;
-import com.google.mlkit.vision.barcode.BarcodeScanner;
-import com.google.mlkit.vision.barcode.BarcodeScanning;
-import com.google.mlkit.vision.common.InputImage;
-
-public class BarcodeScanner {
-    private BarcodeScanner barcodeScanner;
-    
-}
+//package com.cmput301f23t28.casacatalog.Camera;
+//import com.google.mlkit.vision.barcode.Barcode;
+//import com.google.mlkit.vision.barcode.BarcodeScanner;
+//import com.google.mlkit.vision.barcode.BarcodeScanning;
+//import com.google.mlkit.vision.common.InputImage;
+//
+//public class BarcodeScanner {
+//    private BarcodeScanner barcodeScanner;
+//
+//}
