ae592457145a3e64324f8b7ae43b26c48b00d619
Ryan Bott <bott.ryan@gmail.com>
2023-12-04 10:23:32 -0700

    Some basic unit tests for Filter.

--- CasaCatalog/app/src/test/java/com/cmput301f23t28/casacatalog/helpers/FilterTest.java
+++ CasaCatalog/app/src/test/java/com/cmput301f23t28/casacatalog/helpers/FilterTest.java
@@ -6,13 +6,22 @@ import junit.framework.TestCase;
 
 import java.util.function.Predicate;
 
+/**
+ * Tests for the Filter class
+ */
 public class FilterTest extends TestCase {
 
+    /**
+     * Tests the getCurrentType method
+     */
     public void testGetCurrentType() {
         Filter filter = new Filter("date", "", "hello", "you");
         assertEquals(Filter.Type.date, filter.getCurrentType());
     }
 
+    /**
+     * Tests the setCurrentType method
+     */
     public void testSetCurrentType() {
         String type = "description";
         Filter filter = new Filter("date", "", "hello", "you");
@@ -20,23 +29,35 @@ public class FilterTest extends TestCase {
         assertEquals(filter.getCurrentType(), Filter.Type.description);
     }
 
+    /**
+     * Tests the getCurrentFilterType method
+     */
     public void testGetCurrentFilterType() {
         Filter filter = new Filter("date", "equals", "hello", "you");
         assertEquals(Filter.FilterType.equals, filter.getCurrentFilterType());
     }
 
+    /**
+     * Tests the setCurrentFilterType method
+     */
     public void testSetCurrentFilterType() {
         Filter filter = new Filter("date", "equals", "hello", "you");
         filter.setCurrentFilterType("notequals");
         assertEquals(filter.getCurrentFilterType(), Filter.FilterType.notequals);
     }
 
+    /**
+     * Tests the getVal1 method
+     */
     public void testGetVal1() {
         String val1 = "burger";
         Filter filter = new Filter("date", "", val1, "you");
         assertEquals(val1, filter.getVal1());
     }
 
+    /**
+     * Tests the setVal1 method
+     */
     public void testSetVal1() {
         String val1 = "burger";
         String newVal1 = "cheeseburger";
@@ -45,12 +66,18 @@ public class FilterTest extends TestCase {
         assertEquals(newVal1, filter.getVal1());
     }
 
+    /**
+     * Tests the getGet2 method
+     */
     public void testGetVal2() {
         String val2 = "burger";
         Filter filter = new Filter("date", "", "val1", val2);
         assertEquals(val2, filter.getVal2());
     }
 
+    /**
+     * Tests the setVal2 method
+     */
     public void testSetVal2() {
         String val2 = "burger";
         String newVal2 = "cheeseburger";
@@ -59,6 +86,9 @@ public class FilterTest extends TestCase {
         assertEquals(newVal2, filter.getVal2());
     }
 
+    /**
+     * Tests the getFilterPredicate method
+     */
     public void testGetFilterPredicate() {
         // set up an item
         Item item = new Item();
