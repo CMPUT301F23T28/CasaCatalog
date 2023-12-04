fa4269a65a938dbdd4726b8853bf08b9ee731244
Ryan Bott <bott.ryan@gmail.com>
2023-12-03 20:43:01 -0700

    Added some UI tests. Set up multi select for applying tags on the main screen.

--- CasaCatalog/app/src/androidTest/java/com/cmput301f23t28/casacatalog/util/EspressoUtils.java
+++ CasaCatalog/app/src/androidTest/java/com/cmput301f23t28/casacatalog/util/EspressoUtils.java
@@ -2,11 +2,27 @@ package com.cmput301f23t28.casacatalog.util;
 
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.isA;
+import static org.hamcrest.core.Is.is;
+
+import android.view.View;
+import android.widget.Checkable;
+
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.matcher.ViewMatchers;
 
 import junit.framework.AssertionFailedError;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 
 public class EspressoUtils {
     public static ViewInteraction waitUntilVisible(ViewInteraction viewInteraction, long maxMillis) {
@@ -21,5 +37,67 @@ public class EspressoUtils {
             }
         }
         throw new AssertionFailedError("View not visible within timeout");
+    }
+
+    /**
+     * https://stackoverflow.com/questions/37819278/android-espresso-click-checkbox-if-not-checked
+     * @param checked condition of a checkbox
+     */
+    public static ViewAction setChecked(final boolean checked) {
+        return new ViewAction() {
+            @Override
+            public BaseMatcher<View> getConstraints() {
+                return new BaseMatcher<View>() {
+                    @Override
+                    public boolean matches(Object item) {
+                        return isA(Checkable.class).matches(item);
+                    }
+
+                    @Override
+                    public void describeMismatch(Object item, Description mismatchDescription) {}
+
+                    @Override
+                    public void describeTo(Description description) {}
+                };
+            }
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                Checkable checkableView = (Checkable) view;
+                checkableView.setChecked(checked);
+            }
+        };
+
+
+    }
+    /** This Matcher helps us to perform actions on Views in RecyclerViews that we don't have another way of accessing.
+     *  It was originally needed to perform click operations on checkboxes in a RecyclerView.
+     * https://stackoverflow.com/a/43552179/255217
+     */
+    public static Matcher<View> getElementFromMatchAtPosition(final Matcher<View> matcher, final int position) {
+        return new BaseMatcher<View>() {
+            int counter = 0;
+            @Override
+            public boolean matches(final Object item) {
+                if (matcher.matches(item)) {
+                    if(counter == position) {
+                        counter++;
+                        return true;
+                    }
+                    counter++;
+                }
+                return false;
+            }
+
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("Element at hierarchy position "+position);
+            }
+        };
     }
 }
--- CasaCatalog/app/src/androidTest/java/com/cmput301f23t28/casacatalog/views/MainActivityTest.java
+++ CasaCatalog/app/src/androidTest/java/com/cmput301f23t28/casacatalog/views/MainActivityTest.java
@@ -1,14 +1,26 @@
 package com.cmput301f23t28.casacatalog.views;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.longClick;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.core.AllOf.allOf;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -16,6 +28,12 @@ import androidx.test.filters.LargeTest;
 import com.cmput301f23t28.casacatalog.R;
 import com.cmput301f23t28.casacatalog.util.EspressoUtils;
 
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,12 +48,24 @@ public class MainActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<>(MainActivity.class);
 
+    @Before
+    public void beforeTest() {
+        Intents.init();
+    }
+
+    @After
+    public void afterTest() {
+        Intents.release();
+    }
+
+    private int maxRandom = 1000;
+
     /**
      * Test for deleting single item.
      */
     @Test
     public void testDeleteSingleItem() {
-        int rand = new Random().nextInt(99);
+        int rand = new Random().nextInt(maxRandom);
         String name = "testDeletingItem" + rand;
         addTestItemTestData(name);
         try {
@@ -53,11 +83,11 @@ public class MainActivityTest {
      */
     @Test
     public void testDeleteMultiItem() {
-        int rand = new Random().nextInt(99);
+        int rand = new Random().nextInt(maxRandom);
         String name = "testDeletingItem" + rand;
         addTestItemTestData(name);
 
-        rand = new Random().nextInt(99);
+        rand = new Random().nextInt(maxRandom);
         String name2 = "testDeletingItem" + rand;
         addTestItemTestData(name2);
         try {
@@ -72,6 +102,38 @@ public class MainActivityTest {
         onView(withText(name2)).check(doesNotExist());
     }
 
+    @Test
+    public void testChangeTag() {
+        int rand = new Random().nextInt(maxRandom);
+        String name = "test" + rand;
+        addTestItemTestData(name);
+
+        onView(withText(name)).perform(ViewActions.scrollTo(), longClick());
+        onView(withId(R.id.add_tag_items_button)).perform(click());
+
+        onView(allOf(EspressoUtils.getElementFromMatchAtPosition(allOf(withId(R.id.tagCheckBox)), 0), isDisplayed())).perform(click());
+        onView(withContentDescription("Navigate up")).perform(click());
+
+        onView(withText(name)).perform(click());
+        onView(withId(R.id.itemTagsList)).check(matches(hasChildCount(1)));
+
+
+        removeTestItem(name);
+
+    }
+    @Test
+    public void testNavigateToSetTagActivity() {
+        int rand = new Random().nextInt(maxRandom);
+        String name = "test" + rand;
+        addTestItemTestData(name);
+
+        onView(withText(name)).perform(ViewActions.scrollTo(), longClick());
+        onView(withId(R.id.add_tag_items_button)).perform(click());
+        onView(withText("Edit tags")).check(matches(isDisplayed()));
+
+        removeTestItem(name);
+    }
+
     /**
      * TODO: Refactor so the code is not duplicated in other tests.
      * Adds an item to the database.
@@ -84,4 +146,11 @@ public class MainActivityTest {
         onView(withId(R.id.addItemToListBtn)).perform(ViewActions.scrollTo());
         onView(withId(R.id.addItemToListBtn)).perform(click());
     }
+
+    public void removeTestItem(String name) {
+        onView(withText(name)).perform(ViewActions.scrollTo(), click());
+        onView(withText("Delete item")).perform(ViewActions.scrollTo(), click());
+    }
+
+
 }
