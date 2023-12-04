e36c8ac6738952a88befc3d21c2dd2a8309f7430
jadc <chehimj@gmail.com>
2023-12-04 12:37:30 -0700

    cleanup unused methods

--- CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/helpers/ItemHolder.java
+++ CasaCatalog/app/src/main/java/com/cmput301f23t28/casacatalog/helpers/ItemHolder.java
@@ -24,20 +24,18 @@ import java.util.ArrayList;
  * A RecyclerView ViewHolder that stores all data related to rendering an item in the ItemList
  */
 public class ItemHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener, View.OnClickListener {
-    private TextView ItemName;
-    private TextView ItemPurchaseDate;
-    private TextView ItemPrice;
-    private ImageView ItemImage;
-    private ChipGroup ItemTags;
-    private ListClickListener mListener;
-    private ConstraintLayout ItemContainer;
-    private int backgroundColor = Color.WHITE;
-    private Context context;
+    private final TextView ItemName;
+    private final TextView ItemPurchaseDate;
+    private final TextView ItemPrice;
+    private final ImageView ItemImage;
+    private final ChipGroup ItemTags;
+    private final ListClickListener<ItemHolder> mListener;
+    private final ConstraintLayout ItemContainer;
     /**
      * Constructor for ItemHolder. Basically connects the UI elements to an in code reference.
      * @param itemView The item view.
      */
-    public ItemHolder(@NonNull View itemView, ListClickListener listener) {
+    public ItemHolder(@NonNull View itemView, ListClickListener<ItemHolder> listener) {
         super(itemView);
 
         this.mListener = listener;
@@ -48,7 +46,6 @@ public class ItemHolder extends RecyclerView.ViewHolder implements View.OnLongCl
         ItemPrice = itemView.findViewById(R.id.ItemPrice);
         ItemTags = itemView.findViewById(R.id.ItemTags);
         ItemImage = itemView.findViewById(R.id.ItemImage);
-        context = itemView.getContext();
 
         itemView.setOnClickListener(this);
         itemView.setOnLongClickListener(this);
@@ -80,7 +77,7 @@ public class ItemHolder extends RecyclerView.ViewHolder implements View.OnLongCl
      */
     public void setItemImage(String imageURL) {
         Log.d("SETTING ITEM IMAGE", imageURL);
-        if (imageURL != "") {
+        if (!imageURL.trim().isEmpty()) {
             Picasso.get()
                     .load(imageURL)
                     .placeholder(itemView.getContext().getResources().getDrawable(R.drawable.ic_launcher_foreground))//it will show placeholder image when url is not valid.
@@ -147,15 +144,4 @@ public class ItemHolder extends RecyclerView.ViewHolder implements View.OnLongCl
         }
     }
 
-    /**
-     * Gets the selected style color as an int.
-     * @return
-     */
-    public int getSelectedStyle() {
-        Drawable background = ItemContainer.getBackground();
-        if (background instanceof ColorDrawable) {
-            return ((ColorDrawable) background).getColor();
-        }
-        return 0;
-    }
 }
